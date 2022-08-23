c16481a16b9609e29d80cb527610645b2dc0c1b5
bortkyle18 <bortkyle18@gmail.com>
2022-08-23 12:56:35 -0400

    project descriptions update

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -19,6 +19,9 @@ const Work = (props) => {
               </a>
             </div>
             <p>Express/MySQL/JavaScript</p>
+            <br/>
+            <br/>
+            <h5>This application makes travel planning easier and also lets you cherish past traveling memories. Plan your trip, add to your wishlist, and save your memories.</h5>
           </div>
           <div className="sampleText" id="note-taker">
             <a href="https://young-meadow-06036.herokuapp.com/" target="_blank" rel="noreferrer">
@@ -30,6 +33,9 @@ const Work = (props) => {
               </a>
             </div>
             <p>Express</p>
+            <br/>
+            <br/>
+            <h5>An application that can be used to write, save, and delete notes using node.js and express.js.</h5>
           </div>
           <div className="sampleText" id="social-networking-nosql">
             <a href="https://github.com/bortkyle18/social-network-api-nosql" target="blank" rel="noreferrer">
@@ -41,6 +47,9 @@ const Work = (props) => {
               </a>
             </div>
             <p>Mongodb/Mongoose</p>
+            <br/>
+            <br/>
+            <h5>An API for a social network web application where users can share their thoughts, react to their friends' thoughts, and create a friend list.</h5>
           </div>
           <div className="sampleText" id="employee-tracker">
             <a href="https://github.com/bortkyle18/employee-tracker" target="blank" rel="noreferrer">
@@ -52,6 +61,9 @@ const Work = (props) => {
               </a>
             </div>
             <p>Node/SQL</p>
+            <br/>
+            <br/>
+            <h5>A command-line application for managing a company's employees using node, inquirer, and MySQL.</h5>
           </div>
           <div className="sampleText" id="run-buddy">
             <a href="https://bortkyle18.github.io/run-buddy/" target="blank" rel="noreferrer">
@@ -63,6 +75,9 @@ const Work = (props) => {
               </a>
             </div>
             <p>HTML/CSS</p>
+            <br/>
+            <br/>
+            <h5>This is a professional web-friendly designed website for a fictional business called Run Buddy Inc that offers personal training services.</h5>
           </div>
         </article>
       </section>
