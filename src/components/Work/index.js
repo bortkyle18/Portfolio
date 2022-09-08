982e34e56c7017bb0f2bf5f0c56c9f9e3a1a0885
bortkyle18 <bortkyle18@gmail.com>
2022-09-08 16:24:45 -0400

    update with book den

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -9,6 +9,20 @@ const Work = (props) => {
       <section id="workPage" className="portfolio-section">
         <h2>Work</h2>
         <article>
+          <div className="sampleText" id="book-den">
+            <a href="https://glacial-ridge-11514.herokuapp.com/" target="blank" rel="noreferrer">
+              The Book Den
+            </a>
+            <div>
+              <a href="https://github.com/bortkyle18/book-den" target="_blank" rel="noreferrer" className="gitLink">
+                <img src={ gitHub } alt="github logo"/>
+              </a>
+            </div>
+            <p>MERN Stack</p>
+            <br/>
+            <br/>
+            <h5>A social media website for book lovers. Keep your own Library consisting of wishlist books, favorite books, and other books on your bookshelf. Leave reviews and comment on reviews with your thoughts!</h5>
+          </div>
           <div className="sampleText" id="travelot">
             <a href="https://travelot-app.herokuapp.com/" target="_blank" rel="noreferrer">
               Travelot App
@@ -64,20 +78,6 @@ const Work = (props) => {
             <br/>
             <br/>
             <h5>A command-line application for managing a company's employees using node, inquirer, and MySQL.</h5>
-          </div>
-          <div className="sampleText" id="run-buddy">
-            <a href="https://bortkyle18.github.io/run-buddy/" target="blank" rel="noreferrer">
-              Run Buddy
-            </a>
-            <div>
-              <a href="https://github.com/bortkyle18/run-buddy" target="_blank" rel="noreferrer" className="gitLink">
-                <img src={ gitHub } alt="github logo"/>
-              </a>
-            </div>
-            <p>HTML/CSS</p>
-            <br/>
-            <br/>
-            <h5>This is a professional web-friendly designed website for a fictional business called Run Buddy Inc that offers personal training services.</h5>
           </div>
         </article>
       </section>
